dccb5ad7b969a224769ab4719419609cbcaffcf4
michal dub <michal.dub@sanomamedia.cz>
2015-01-27 15:46:09 +0100

    fixed tests for django 1.7

--- test_app_data/__init__.py
+++ test_app_data/__init__.py
@@ -1,11 +1,20 @@
+import django
+
 test_runner = None
 old_config = None
 
 def setup():
     global test_runner
     global old_config
-    from django.test.simple import DjangoTestSuiteRunner
-    test_runner = DjangoTestSuiteRunner()
+    try:
+        from django.test.runner import DiscoverRunner as TestRunner
+    except ImportError:
+        from django.test.simple import DjangoTestSuiteRunner as TestRunner
+    try:
+        django.setup()
+    except AttributeError:
+        pass
+    test_runner = TestRunner()
     test_runner.setup_test_environment()
     old_config = test_runner.setup_databases()
 
